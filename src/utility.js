84c3c09a48eb02fd5de0e723846c9ffe4bba9d0f
nima soufiloo <nima.soufiloo@gmail.com>
2020-07-24 19:03:16 +0430

    completed messages

--- src/utility.js
+++ src/utility.js
@@ -30,16 +30,17 @@ module.exports = {
           fa: "زمان اتصال منقضی شد.",
           en: "Connection timed out.",
         };
-      case "ENOTFOUND":
-        return {
-          fa: "آدرس سرور مورد نظر یافت نشد.",
-          en: "Server address not found.",
-        };
       case 200:
         return {
           fa: "درخواست شما با موفقیت اجرا گردید.",
           en: "Operation has been completed successfully.",
         };
+      case 201:
+        return {
+          fa: "درخواست شما با موفقیت اجرا گردید.",
+          en: "Operation has been completed successfully.",
+        };
+
       case 204:
         return {
           fa: "درخواست شما با موفقیت اجرا گردید.",
@@ -55,15 +56,60 @@ module.exports = {
           fa: "احراز هویت با خطا مواجه شد.",
           en: "Authentication failed.",
         };
+      case 403:
+        return {
+          fa: "درخواست شما با خطای درسترسی مواجه شد.",
+          en: "You have insufficient permission to apply your request.",
+        };
+
       case 404:
         return {
           fa: "رکورد مورد نظر یافت نشد.",
           en: "Specified record not found.",
         };
+      case 405:
+        return {
+          fa: "درخواست شما معتبر نمی باشد.",
+          en: "Your request is not valid.",
+        };
+      case 412:
+        return {
+          fa: "درخواست شما قابل اجرا نمی باشد.",
+          en: "Your request cannot be applied.",
+        };
+      case 413:
+        return {
+          fa: "درخواست حجم فرستاده شده  در درخواست بزرگتر از حد مجاز می باشد.",
+          en: "Request length is too large to process.",
+        };
+      case 429:
+        return {
+          fa: "تعداد درخواست های شما بیش از حد مجاز می باشد.",
+          en: "You have sent too many requests.",
+        };
+
       case 500:
         return {
           fa: "خطایی در داخل سرور رخ داده است.",
           en: "Internal server error occurred.",
+        };
+
+      case 501:
+        return {
+          fa: "درخواست شما پیاده سازی نشده است.",
+          en: "Your request is not implemented.",
+        };
+
+      case 503:
+        return {
+          fa: "وب سرویس مورد نظر در حال سرویس دهی نمی باشد.",
+          en: "Web API service is not available.",
+        };
+
+      case "ENOTFOUND":
+        return {
+          fa: "آدرس سرور مورد نظر یافت نشد.",
+          en: "Server address not found.",
         };
       default:
         return {
