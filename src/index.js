74a74529b2aba7baa86b4308285bc89b68222ed1
nima soufiloo <nima.soufiloo@gmail.com>
2020-07-26 16:43:54 +0430

    fixed the package.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
 const Credential = require("./Credential");
 const QueryBuilder = require("./QueryBuilder");
-const credential = new Credential(
-  "http://192.168.10.100/iromart",
-  "crmadmin",
-  "Crm1234@@",
-  "iromart"
-);
-const Do = async () => {
-  const res = await new QueryBuilder(credential)
-    .execute("WhoAmI", "get")
-    .send();
-  // const res = await new QueryBuilder(credential).get("accounts").top(1).send();
-  console.log(res);
-};
-
 module.exports = { Credential, QueryBuilder };
-Do();
